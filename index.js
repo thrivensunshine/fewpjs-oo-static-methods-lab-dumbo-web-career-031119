7d35b1940011122c381003ec85ea476fdea3e845
DANIELLE ALBA <danib.sgh@gmail.com>
2019-05-03 17:17:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 class Formatter {
-  //add static methods here
-  static capitalize(str){
-    return  str.charAt(0).toUpperCase() + str.slice(1);
+  static capitalize( string ) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
   }
-  
-  static sanitize(str){
-    let newStr = str.replace(/[^-,^' ',A-Za-z0-9]+/g, '');
-  return newStr
-    }
-    
- static titleize(str){
-      let specWords = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'form']
-      let strArr = str.split(' ')
-     
-    for(let i = 0; i < strArr.length; i++){
-       strArr[i][0] = strArr[i][0].toUpperCase()
-      for(let j = 0; j < specWords.length; j++){
-        if (strArr[i] === specWords[j]){
-        
-         strArr[i] = strArr[i].toUpperCase()
-          
+
+  static sanitize( string ) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = sentence.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
         }
       }
+
     }
-    return strArr.join(' ', ' ')
-    }
+    return result.join( " " );
+  }
 }
