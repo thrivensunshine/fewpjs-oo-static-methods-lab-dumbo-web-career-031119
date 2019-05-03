5bf9e0c3cff6a96b1c142608b558833a2fd4d1f5
DANIELLE ALBA <danib.sgh@gmail.com>
2019-05-03 17:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return  str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  static sanitize(str){
+    let newStr = str.replace(/[^-,^' ',A-Za-z0-9]+/g, '');
+  return newStr
+    }
+    
+ static titleize(str){
+      let specWords = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'form']
+      let strArr = str.split(' ')
+     
+    for(let i = 0; i < strArr.length; i++){
+       strArr[i][0] = strArr[i][0].toUpperCase()
+      for(let j = 0; j < specWords.length; j++){
+        if (strArr[i] === specWords[j]){
+        
+         strArr[i] = strArr[i].toUpperCase()
+          
+        }
+      }
+    }
+    return strArr.join(' ', ' ')
+    }
 }
